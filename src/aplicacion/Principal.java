6feb31bab829c33a5eb0565846f9930fad68446a
Álvaro <ainiguezdislaa@gmail.com>
2023-02-20 14:58:13 +0100

    ejercicio1 terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,13 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import dominio.ej2_1;
+import java.util.Scanner;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un numero");
+        int num = sc.nextInt();
+        System.out.println("Suma desde 1 hasta " + num + " = " + ej2_1.suma(num));
+
+
+    }
 }
--- src/dominio/ej2_1.java
+++ src/dominio/ej2_1.java
@@ -1,2 +1,16 @@
-package dominio;public class ej2_1 {
+package dominio;
+import java.util.Scanner;
+
+public class ej2_1 {
+
+
+    public	static int suma(int n) {
+        if(n<=1) {
+            return n;
+
+        }else {
+            return n+ suma(n-1);
+        }
+    }
+
 }
